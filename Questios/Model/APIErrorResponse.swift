34744e3e173572b47e1276060d10127e9e5f1a4f
David <david_gunawan_@hotmail.com>
2024-06-04 11:36:19 +0700

    Add Error Response

--- Questios/Model/APIErrorResponse.swift
+++ Questios/Model/APIErrorResponse.swift
@@ -1,8 +1,14 @@
 //
-//  ErrorResponse.swift
+//  APIErrorResponse.swift
 //  Questios
 //
 //  Created by David Gunawan on 04/06/24.
 //
 
 import Foundation
+
+// MARK: - APIErrorResponse
+struct APIErrorResponse: APIError {
+    let message: String
+    let statusCode: Int
+}
